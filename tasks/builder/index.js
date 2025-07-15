ee6d934787f3d81c0ab839b5bee9f18ad9ec0ef1
kholstinin <kholstinin.da@gmail.com>
2025-07-16 02:26:02 +0500

    feat: task-3

--- tasks/builder/index.js
+++ tasks/builder/index.js
@@ -1,8 +1,8 @@
-import fs from 'node:fs';
+import fs from "node:fs";
 
 import { bundle } from "./bundle.js";
 
 const output = bundle(process.argv[2]);
 
-fs.mkdirSync('./dist');
+fs.mkdirSync("./dist", { recursive: true });
 fs.writeFileSync(`./dist/main.js`, output);
