c9ebf844550b63253cf0055b6e9311563d245ec6
mihamix@yandex.ru <mihamix@yandex.ru>
2025-08-01 20:13:09 +0700

    fix:task 5 - rollup

--- tasks/project/rollup.config.js
+++ tasks/project/rollup.config.js
@@ -5,5 +5,35 @@ import html from "@rollup/plugin-html";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  
+  input: "src/index.tsx",
+  output: {
+    file: "dist/rollup/index.js",
+    format: "esm",
+  },
+  jsx: 'react-jsx',
+  plugins: [
+    nodeResolve({browser: true}),
+    commonjs(),
+    typescript(),
+    css(),
+    html({
+      title: 'TODO App',
+      publicPath: '/rollup/',
+      template: ({files, publicPath, title}) => {
+        return `
+          <html>
+            <head>
+              <meta charset="utf-8">
+              <title>${title}</title>
+              ${files.css?.map(file => `<link rel="stylesheet" href="${publicPath}${file.fileName}">`).join('\n')}
+            </head>
+            <body>
+              <div id="root"></div>
+              ${files.js?.map(file => `<script type="module" src="${publicPath}${file.fileName}"></script>`).join('\n')}
+            </body>
+          </html>
+        `
+      },
+    }),
+  ],
 };
