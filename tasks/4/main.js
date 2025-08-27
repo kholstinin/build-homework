58e71c3d885a5f209989c3b3158c0bf4524f094a
MaratKhambelov <maratkhambelov@gmail.com>
2025-08-27 14:54:12 +0300

    task 4 - Target, Environment, транспиляция и полифиллы

--- tasks/4/main.js
+++ tasks/4/main.js
@@ -4,12 +4,14 @@ import * as astring from "astring";
 import { parse } from "acorn";
 
 // read file with source code
+const source = fs.readFileSync('./entry.js', 'utf-8')
 
 // get ast from source code
 
+const parsedAst = parse(source, {ecmaVersion: 2020, sourceType: 'module'})
 // transform ast
-
+transformer(parsedAst)
 // convert ast to source code
-
+const result = astring.generate(parsedAst)
 // write source code to file
 fs.writeFileSync("./result.js", result);
--- tasks/4/transformer.js
+++ tasks/4/transformer.js
@@ -2,5 +2,13 @@ import { walk } from 'estree-walker'
 
 // should return transformed ast
 export function transformer(ast) {
-  
+    walk(ast, {
+        enter(node){
+            if(node.type === 'VariableDeclaration') {
+                if( node.kind === 'const'  || node.kind === 'let'){
+                    node.kind = 'var'
+                }
+            }
+        }
+    })
 }
