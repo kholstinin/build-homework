f9ac8e25f5c36d686e2a9f1a00745e3cacb44402
MaratKhambelov <maratkhambelov@gmail.com>
2025-08-30 15:12:07 +0300

    Task 5: современные сборщики - webpack, esbuild

--- tasks/project/esbuild.ts
+++ tasks/project/esbuild.ts
@@ -1,7 +1,40 @@
-import esbuild from 'esbuild';
+import esbuild from "esbuild";
+import path from "node:path";
+import { type BuildOptions } from 'esbuild'
+import { htmlPlugin }  from '@craftamap/esbuild-plugin-html';
 
-const options = {
+const options: BuildOptions = {
+  bundle: true,
+  entryPoints: ["./src/index.tsx"],
+  outdir: path.resolve(import.meta.dirname, `./dist/esbuild/`),
+  resolveExtensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
+  publicPath: "/esbuild/",
+  tsconfig: "./tsconfig.json",
+  plugins: [
+    htmlPlugin({
+      files: [
+        {
+          entryPoints: [
+            "src/index.tsx",
+          ],
+          filename: 'index.html',
+          htmlTemplate: `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+                <div id="root">
+                </div>
+            </body>
+            </html>
+          `,
+        },
+      ]
+    })
+  ]
+};
 
-}
-
-esbuild.build(options).catch(() => process.exit(1))
+esbuild.build(options).catch(() => process.exit(1));
--- tasks/project/webpack.config.ts
+++ tasks/project/webpack.config.ts
@@ -4,7 +4,65 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
 const config: Configuration = {
-  
+  entry: "./src/index.tsx", // path.resolve(import.meta.dirname, "./src/index.tsx"),
+  mode:  process.env.NODE_ENV === 'development' ? 'development'  : 'production',
+  target: ["web"],
+  experiments: {
+    css: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "swc-loader",
+          options: {
+            "jsc": {
+              "parser": {
+                "syntax": "typescript",
+                "tsx": true,
+              },
+              transform: {
+                react: {
+                  runtime: "automatic", 
+                  importSource: "react",
+                },
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: "./tsconfig.json" ,
+      }),
+    ],
+  },
+  output: {
+    path: path.resolve(import.meta.dirname, `./dist/webpack/`),
+    publicPath: "/webpack/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent:`
+          <!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <title>My App</title>
+            </head>
+            <body>
+              <div id="root"></div>
+            </body>
+          </html>
+      `,
+    }),
+  ],
 };
 
 export default config;
