f95434810d1f17179560742fe56ca423b48a38ce
MaratKhambelov <maratkhambelov@gmail.com>
2025-09-04 19:47:53 +0300

    feat: task 5

--- tasks/project/rollup.config.js
+++ tasks/project/rollup.config.js
@@ -3,7 +3,56 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import html from "@rollup/plugin-html";
 import typescript from "@rollup/plugin-typescript";
+import swc from "@rollup/plugin-swc";
 
 export default {
-  
+  input: './src/index.tsx',
+  output: {
+    dir: './dist/rollup/',
+    format: 'esm'
+  },
+  replace: ({
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    preventAssignment: true,
+  }),
+  plugins: [
+    css(),
+    nodeResolve(), 
+    commonjs({
+        transformMixedEsModules: true, extensions: [".tsx", ".ts", ".jsx", ".js"]
+    }),
+    typescript({
+        tsconfig: "./tsconfig.json"
+    }),
+    swc(),
+    html({
+        publicPath: "/rollup/",
+        template: ({files, publicPath}) => {
+        const scripts = (files.js || [])
+            .map(({ fileName }) => `<script type="module" src="${publicPath}${fileName}"></script>`)
+            .join("\n");
+        const links = (files.css || [])
+            .map(({ fileName }) => `<link rel="stylesheet" href="${publicPath}${fileName}">`)
+            .join("\n");
+
+            console.log(links);
+
+            return `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                ${links}
+                ${scripts}
+            </head>
+            <body>
+                <div id="root">
+                </div>
+            </body>
+            </html>
+          `
+        },
+        }),
+  ]
 };
--- tasks/project/vite.config.ts
+++ tasks/project/vite.config.ts
@@ -1,5 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  
-})
+  base: "/vite/",
+  plugins: [tsconfigPaths()],
+  build: {
+    outDir: path.resolve(import.meta.dirname, `./dist/vite/`),
+  },
+});
