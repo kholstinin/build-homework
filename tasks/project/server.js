b243f9bf32d9b46b74e709484def00d3aa4175dd
kholstinin <kholstinin.da@gmail.com>
2025-09-05 15:08:47 +0500

    feat: 16

--- tasks/project/server.js
+++ tasks/project/server.js
@@ -1,7 +1,12 @@
 import path from "node:path";
 import fs from "node:fs";
+import crypto from "node:crypto";
 import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
+
+function generateCSP(nonce) {
+  return [`script-src 'nonce-${nonce}'`].join(" ");
+}
 
 const fastify = Fastify({
   logger: true,
@@ -12,18 +17,29 @@ fastify.register(fastifyStatic, {
   list: true,
 });
 
-function htmlReply(reply, bundler) {
-  const html = fs.readFileSync(`./dist/${bundler}/index.html`, "utf-8");
+function htmlReply(reply, html) {
   reply.type("text/html; charset=utf-8");
-
   return html;
+}
+
+function nonceHtmlReply(reply, html) {
+  const nonce = crypto.randomBytes(16).toString("hex");
+  reply.header("content-security-policy", generateCSP(nonce));
+  reply.type("text/html; charset=utf-8");
+  return html.replaceAll("{{NONCE_VALUE}}", nonce);
 }
 
 const bundlers = ["webpack", "esbuild", "rollup", "vite"];
 
 bundlers.forEach((bundler) => {
   fastify.get(`/${bundler}`, async function handler(_request, reply) {
-    return htmlReply(reply, bundler);
+    const html = fs.readFileSync(`./dist/${bundler}/index.html`, "utf-8");
+    return htmlReply(reply, html);
+  });
+
+  fastify.get(`/${bundler}-csp`, async function handler(_request, reply) {
+    const html = fs.readFileSync(`./dist/${bundler}/index.html`, "utf-8");
+    return nonceHtmlReply(reply, html);
   });
 });
 
